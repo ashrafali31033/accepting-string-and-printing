69ec89bea2cc821dcc7f566062779b2619732da7
ashrafali31033 <ashrafali31033@gmail.com>
2024-12-02 16:12:49 +0530

    Add files via upload

---
+++ ashraf3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ashraf3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a= sc.next();
+        int b=sc.nextInt();
+        String c=String.format("name of a person is %s and age is %d",a,b);
+        System.out.println(c);
+    }
+}
